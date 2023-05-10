dd77cfbcf64f833ddcd2ee36b491e805c78f12d4
Bryce Fisher <101945315+bryceefisher@users.noreply.github.com>
2023-05-09 19:07:08 -0700

    Add files via upload

---
+++ Eliza.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApp6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("What would you like to talk about? Enter your response here or Q to quit: ");
+        string statement = Console.ReadLine();
+
+        while (statement.ToLower() != "q")
+        {
+            Console.WriteLine($"Psychologist: {CreateElizaResponse(statement)}");
+            Console.Write("Patient: ");
+            statement = Console.ReadLine();
+            Thread.Sleep(1000);
+        }
+    }
+    static string CreateElizaResponse(string statement)
+    {
+        if (statement.ToLower().Contains("my"))
+        {
+            return ("Tell me more about your " + statement.Substring(statement.IndexOf("my") + 3).Trim() + ".");
+        }
+        else if (statement.ToLower().Contains("love") || statement.ToLower().Contains("hate"))
+        {
+            return "You have strong feelings about that!";
+        }
+        else
+        {
+            string[] responseArray = { "Please go on.", "Tell me more.", "Continue..." };
+            Random random = new Random();
+            return responseArray[random.Next(0, responseArray.Length)];
+        }
+    }
+
+}
+
+
+
